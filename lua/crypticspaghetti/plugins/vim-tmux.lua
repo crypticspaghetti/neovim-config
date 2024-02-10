86d550170c10f73e1fbb4a83650a28839b6ac592
Cryptic Spaghetti <crypticspaghetti@protonmail.com>
2024-02-10 15:29:31 -0600

    Add neovim and tmux itegration plugin

---
+++ lua/crypticspaghetti/plugins/vim-tmux.lua
@@ -0,0 +1,19 @@
+-- For working with both tmux and (neo)vim
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
+
